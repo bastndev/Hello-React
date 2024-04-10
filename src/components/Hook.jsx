39772d5a0089bd9c39bd0e576da2e5e6373fb227
bastndev <gohit24@gmail.com>
2024-04-10 16:34:34 -0500

    `Add` swap theme

--- src/components/Hook.jsx
+++ src/components/Hook.jsx
@@ -26,7 +26,7 @@ export function Hook() {
 }
  */
 
-// TODO: 2 Second option
+/* // TODO: 2 Second option
 import { useState } from "react";
 
 export function Hook() {
@@ -50,4 +50,48 @@ export function Hook() {
       </button>
     </div>
   );
+} */
+
+// TODO: 3 There option
+import { useState } from "react";
+
+export function Hook() {
+  const [theme, setTheme] = useState(false);
+
+  const swapTheme = () => {
+    setTheme(true);
+    console.log(theme)
+  };
+  return (
+    <div className="flex flex-col">
+      <h1>Theme {theme}</h1>
+      <button onClick={swapTheme} className="bg-green-500 ">
+        Swap Theme
+      </button>
+    </div>
+  );
 }
+
+// Optional
+/* import { useState, useEffect } from "react";
+
+export function Hook() {
+  const [theme, setTheme] = useState(false);
+
+  useEffect(() => {
+    console.log(theme);
+  }, [theme]);
+
+  const swapTheme = () => {
+    setTheme(true);
+  };
+
+  return (
+    <div className="flex flex-col">
+      <h1>Theme {theme}</h1>
+      <button onClick={swapTheme} className="bg-green-500 ">
+        Swap Theme
+      </button>
+    </div>
+  );
+} */
