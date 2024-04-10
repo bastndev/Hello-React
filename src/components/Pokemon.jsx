a854243e9a139d1c0546a71db117611ffd13b37f
bastndev <gohit24@gmail.com>
2024-04-10 15:12:48 -0500

    `Add` Pokemon image and name and title

---
+++ src/components/Pokemon.jsx
@@ -0,0 +1,25 @@
+import "./css/poke.css"
+
+const pokemon = [
+  {
+    id: 1,
+    name: "Pikachu",
+    img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
+  },
+  {
+    id: 2,
+    name: "Charmander",
+    img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+  },
+];
+
+export function Pokemon() {
+  return pokemon.map((item) => {
+    return (
+      <div>
+        <h1>{item.name}</h1>
+        <img className="pokeImg" src={item.img} alt="" />
+      </div>
+    );
+  });
+}
