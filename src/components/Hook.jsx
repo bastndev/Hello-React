f752c88feb5322514502312df4f5d8270dafd9bc
bastndev <gohit24@gmail.com>
2024-04-10 16:25:31 -0500

    `Add` second option remove and add like

--- src/components/Hook.jsx
+++ src/components/Hook.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+//TODO: First Option
+
+/* import { useState } from "react";
 
 export function Hook() {
   const [likes, setLikes] = useState(0);
@@ -17,6 +19,33 @@ export function Hook() {
         onClick={() => setLikes(likes - 1)}
         className="bg-green-500  hover:bg-orange-700"
       >
+        Remove Like -
+      </button>
+    </div>
+  );
+}
+ */
+
+// TODO: 2 Second option
+import { useState } from "react";
+
+export function Hook() {
+  const [likes, setLikes] = useState(0);
+
+  const giveLike = () => {
+    setLikes(likes + 1);
+  };
+  const removeLike = () => {
+    setLikes(likes - 1);
+  };
+  return (
+    <div className="flex flex-col">
+      <h1>Likes {likes}</h1>
+      <button onClick={giveLike} className="bg-green-500  hover:bg-orange-700">
+        Give Like +
+      </button>
+      <br />
+      <button onClick={removeLike} className="bg-green-500 ">
         Remove Like -
       </button>
     </div>
