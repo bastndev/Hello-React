c4094b88cc7418bfd27fcf636420885dff770c52
bastndev <gohit24@gmail.com>
2024-04-10 12:58:13 -0500

    `Add` second option Components

--- src/components/Component.jsx
+++ src/components/Component.jsx
@@ -1,3 +1,11 @@
-export function Component(props) {
+// TODO: option 1
+/* export function Component(props) {
   return <h1>{`Hello ${props.name}`}</h1>;
+}
+ */
+
+// Second Option
+
+export function Component({ name }) {
+  return <h1>{`Hello ${name}`}</h1>;
 }
