b61a2fb89000ed7865fdd6be23c760e471477625
bastndev <gohit24@gmail.com>
2024-04-09 21:55:50 -0500

    `Add` css in react

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import './App.css'; // Suponiendo que los estilos están en App.css
 
 function App() {
   return (
     <div className="App">
-      <h1 class="text-black">Hello World!</h1>
+      <h1 className="h1 text-black">Hello World!</h1>
     </div>
   );
 }
