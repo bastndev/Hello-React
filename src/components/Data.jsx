4a20e9df6a519040ba52ec6851911b1119159205
bastndev <gohit24@gmail.com>
2024-04-09 23:58:40 -0500

    `Add` User and Data

---
+++ src/components/Data.jsx
@@ -0,0 +1,12 @@
+export function Data() {
+  const data = {
+    name: "Gohit",
+    date: 25,
+  };
+  return (
+    <div>
+      <h1>{data.name}</h1>
+      <p>{data.date}</p>
+    </div>
+  );
+}
