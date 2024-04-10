362f6aad7120d572cf18287190c27e1c27bdf1ea
bastndev <gohit24@gmail.com>
2024-04-09 21:53:37 -0500

    `Add` colote text react

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,28 +1,11 @@
 import React from "react";
 
-class SimpleButton extends React.Component {
-  state = {
-    count: 0,
-    backgroundColor: "#1b1c23",
-  };
-
-  handleClick = () => {
-    this.setState((prevState) => ({
-      count: prevState.count + 1,
-      backgroundColor: prevState.count % 2 === 0 ? "#1b1c23" : "#f4f4f4",
-    }));
-  };
-
-  render() {
-    return (
-      <button
-        onClick={this.handleClick}
-        style={{ backgroundColor: this.state.backgroundColor }}
-      >
-        Click me ({this.state.count})
-      </button>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <h1 class="text-black">Hello World!</h1>
+    </div>
+  );
 }
 
-export default SimpleButton;
+export default App;
