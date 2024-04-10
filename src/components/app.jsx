e0fd10b3fd2353f51e4b72a3471ff3952e1bbb29
bastndev <gohit24@gmail.com>
2024-04-09 21:40:54 -0500

    `Add` react

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 
 class SimpleButton extends React.Component {
+  state = {
+    count: 0,
+    backgroundColor: "#1b1c23",
+  };
+
   handleClick = () => {
-    alert("Haz clic en el botón!");
+    this.setState((prevState) => ({
+      count: prevState.count + 1,
+      backgroundColor: prevState.count % 2 === 0 ? "#1b1c23" : "#f4f4f4",
+    }));
   };
 
   render() {
-    return <button onClick={this.handleClick}>Haz clic en mí</button>;
+    return (
+      <button
+        onClick={this.handleClick}
+        style={{ backgroundColor: this.state.backgroundColor }}
+      >
+        Click me ({this.state.count})
+      </button>
+    );
   }
 }
 
