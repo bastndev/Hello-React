d458aeacdc6ba88115f6d781dcb26cf88346f018
bastndev <gohit24@gmail.com>
2024-04-10 14:53:18 -0500

    `Add` Card to project

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import "./css/card.css";
 
 export function Card({ state }) {
   return (
-    <div className="card">
+    <div className="card rounded-md">
       <h1>My first card</h1>
       <p className={state ? "on" : "off"}>Detail card</p>
     </div>
