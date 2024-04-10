1c66283d5667408c98b2bc39a551baf32b2af902
bastndev <gohit24@gmail.com>
2024-04-09 23:29:06 -0500

    `Add` css but use more tailwind

--- src/components/Coffe.jsx
+++ src/components/Coffe.jsx
@@ -1,7 +1,9 @@
+import './css/Coffe.css';
+
 export function Coffe() {
-  const coffeHot = false;
+  const coffeHot = true;
   if (coffeHot) {
-    return <h1>The Coffe is hot</h1>;
+    return <h1 class="text-teal-300">The Coffe is hot</h1>;
   } else {
     return <h1>The Coffe is cold</h1>;
   }
